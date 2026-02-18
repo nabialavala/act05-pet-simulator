f6143b5d7ccf7877b6cd70fad1d3cad0688f69b7
Nabia Lavala <nabialavala@gmail.com>
2026-02-18 15:34:17 -0500

    Part 1 - Auto-Increasing Hunger

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+//part 1 - auto-increasing hunger: import asych library
+import 'dart:async';
 
 void main() {
   runApp(MaterialApp(
@@ -13,48 +15,50 @@ class DigitalPetApp extends StatefulWidget {
 
 class _DigitalPetAppState extends State<DigitalPetApp> {
   String petName = "Your Pet";
-  int happinessLevel = 50;
-  int hungerLevel = 50;
+  double happinessLevel = 50;
+  double hungerLevel = 50;
   //part 1 - Pet Name Customization: initaializing pet name variable
   final TextEditingController _nameController = TextEditingController();
+  //part 1- auto-increasing hunger: creating timer variable
+  Timer? _hungerTimer; //'Timer?' - ? makes the variable able to be assigned null value
 
   void _playWithPet() {
     setState(() {
-      happinessLevel += 10;
+      happinessLevel += 10.0;
       _updateHunger();
     });
   }
 
   void _feedPet() {
     setState(() {
-      hungerLevel -= 10;
+      hungerLevel -= 10.0;
       _updateHappiness();
     });
   }
 
   void _updateHappiness() {
-    if (hungerLevel < 30) {
-      happinessLevel -= 20;
+    if (hungerLevel < 30.0) {
+      happinessLevel -= 20.0;
     } else {
-      happinessLevel += 10;
+      happinessLevel += 10.0;
     }
   }
 
   void _updateHunger() {
     setState(() {
-      hungerLevel += 5;
-      if (hungerLevel > 100) {
-        hungerLevel = 100;
-        happinessLevel -= 20;
+      hungerLevel += 5.0;
+      if (hungerLevel > 100.0) {
+        hungerLevel = 100.0;
+        happinessLevel -= 20.0;
       }
     });
   }
 
 //part 1 - dynamic color change
-  Color _moodColor(int happinessLevel) { //returns a color
-    if (happinessLevel > 70) {
+  Color _moodColor(double happinessLevel) { //returns a color
+    if (happinessLevel > 70.0) {
       return Colors.green;
-    } else if (happinessLevel >= 30) {
+    } else if (happinessLevel >= 30.0) {
       return Colors.yellow;
     } else {
       return Colors.red;
@@ -63,9 +67,9 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
 
 //part 1 - pet mood indicator
   String _moodText() {
-    if (happinessLevel > 70) {
+    if (happinessLevel > 70.0) {
       return "Happy";
-    } else if (happinessLevel >= 30) {
+    } else if (happinessLevel >= 30.0) {
       return "Neutral";
     } else {
       return "Sad";
@@ -73,9 +77,9 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
   }
 
   IconData _moodIcon() {
-    if (happinessLevel > 70) {
+    if (happinessLevel > 70.0) {
       return Icons.sentiment_satisfied_alt;
-    } else if (happinessLevel >= 30) {
+    } else if (happinessLevel >= 30.0) {
       return Icons.sentiment_neutral;
     } else {
       return Icons.sentiment_dissatisfied;
@@ -85,8 +89,27 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
   //part 1 - pet name cutomization: creation method to dispose
   @override
   void dispose() {
+    //part 1 - auto-inc hunger: we need to cancel the timer
+    _hungerTimer?.cancel();
     _nameController.dispose();
     super.dispose();
+  }
+
+  //part 1 - auto-inc hunger: creating funtion to remove hunger every 30 second interval
+  @override
+  void initState() {
+    super.initState();
+    _hungerTimer = Timer.periodic(
+      Duration(seconds: 30),
+      (timer) {
+        setState(() {
+          hungerLevel += 5.0;
+          if (hungerLevel > 100.0) {
+            hungerLevel = 100.0;
+          }
+        });
+      },
+    );
   }
   
 
@@ -127,7 +150,7 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
             SizedBox(height: 16.0),
             Text('Name: $petName', style: TextStyle(fontSize: 20.0)),
             SizedBox(height: 16.0),
-            //part 1 - dynamic color change
+            //part 1 - dynamic color change: adding pet image
             ColorFiltered(
               colorFilter: ColorFilter.mode(
                 _moodColor(happinessLevel),
